1b025f7ac31d045a797b32c5c671a5c8bf03c72a
Lucas <lucas@blueshift.cc>
2023-01-29 19:01:01 -0300

    api integration

--- api/core/index.ts
+++ api/core/index.ts
@@ -1,12 +1,22 @@
 import { APIGatewayEvent, Context } from 'aws-lambda';
+import { Configuration, OpenAIApi } from 'openai';
 import createAPI from 'lambda-api';
 
 const api = createAPI();
+const configuration = new Configuration({
+	apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
 api.get('/', async (req, res) => {
-    return { status: 'Hello from Function!' };
-});
+	const completion = await openai.createCompletion({
+		model: "text-davinci-003",
+		prompt: req.query.prompt,
+		max_tokens: 256
+	});
+	return { status: completion.data.choices[0].text };
+})
 
 export async function handler(event: APIGatewayEvent, context: Context) {
-    return await api.run(event, context);
-};
+	return await api.run(event, context);
+}
